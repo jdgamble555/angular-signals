ce9e8ecdc3818e2ba6e35690d2d07972b83a9035
Jonathan Gamble <jdgamble555@gmail.com>
2023-05-27 23:02:07 -0500

    onpush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, computed, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
   <p>{{ x2() }}</p>
   <button (click)="increment()">Double</button>
