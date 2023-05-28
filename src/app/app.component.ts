301bab8fa78d5e06450a4d95582adfd211799ec7
Jonathan Gamble <jdgamble555@gmail.com>
2023-05-28 00:38:19 -0500

    add links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `<router-outlet></router-outlet>`
+  template: `
+  <router-outlet></router-outlet>
+  <p></p>
+  <ng-container *ngIf="this.router.routerState.snapshot.url === '/rxjs'">
+    <p><a routerLink="/">See Signal Version</a></p>
+  </ng-container>
+  <ng-container *ngIf="this.router.routerState.snapshot.url === '/'">
+    <p><a routerLink="/rxjs">See Rxjs Version</a></p>
+  </ng-container>  
+  `
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(public router: Router) { }
+}
